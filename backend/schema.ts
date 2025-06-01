336f8f584f9bf255e2aaca522a92025ce975fe14
Olayiwola Ayoola <66376919+Layoola@users.noreply.github.com>
2025-06-01 18:25:06 +0000

    task-5/fe-task-2-Fixed last review at time to update if reviewsCount is set above zero

--- backend/schema.ts
+++ backend/schema.ts
@@ -109,11 +109,16 @@ export const lists = {
               );
             }
 
+            // Opting for replacing reviews count with the provided input value
+            // this allows admins to set the reviewsCount value as they see fit and lets customer increment by 1.
+            //
+            // A more restrictive approach would have been to make all calls to increment
+            // the existing value by one regardless of the value that is passes in
             resolvedData.reviewsCount = newReviewsCount;
           },
-          beforeOperation: ({ operation, resolvedData }) => {
+          beforeOperation: ({ resolvedData }) => {
             const newReviewsCount = resolvedData?.reviewsCount;
-            if (newReviewsCount && operation === 'update') {
+            if (newReviewsCount && typeof newReviewsCount === 'number') {
               resolvedData.lastReviewedAt = new Date();
             }
           },
